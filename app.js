95d3638d162c987808b10f7bf41b5a05e8e066da
tonyfang <q710370@gmail.com>
2019-12-23 09:27:46 +0800

    Create home page and Url.js

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const exphbs = require('express-handlebars')
+const crypto = require('crypto')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/record', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 app.get('/', (req, res) => {
-  res.send('短網址')
+
+  let randomString = crypto.randomBytes(32).toString('base64').substr(0, 5)
+  console.log(randomString)
+  res.render('index')
+  console.log(req.body.Url)
+})
+app.post('/', (req, res) => {
+  res.render('index')
+  console.log(req.body.Url)
 })
 
 app.listen(port, () => {
